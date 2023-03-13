80695b8d394f9b199978342a3cb55b03a5e2f00c
Chloe Garza <cmgarza52@gmail.com>
2023-03-13 03:44:27 -0700

    passing tests

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    // we'll fill this in shortly
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string, 10);
+}
